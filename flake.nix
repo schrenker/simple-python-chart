577d6a9ffca018ef3150ef1ffe378a41fede262a
Sebastian Zawadzki <sebastian@zawadzki.tech>
2025-02-26 13:12:19 +0100

    base setup

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ kubernetes-helm yaml-language-server helm-ls yamllint ];
+        };
+      };
+    };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+  };
+}
