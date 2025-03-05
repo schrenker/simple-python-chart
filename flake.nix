a560e93a93d7401b161acb7456666bb0f98470a2
Sebastian Zawadzki <sebastian@zawadzki.tech>
2025-03-05 12:05:41 +0100

    add python to flake.nix for testing

--- flake.nix
+++ flake.nix
@@ -4,7 +4,14 @@
       systems = [ "x86_64-linux" "aarch64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ kubernetes-helm yaml-language-server helm-ls yamllint ];
+          nativeBuildInputs = with pkgs; [ kubernetes-helm yaml-language-server helm-ls yamllint python313 ];
+          shellHook = ''
+            python3.13 -m venv .venv
+            source .venv/bin/activate
+
+            pip -q install pyright black requests kubernetes prometheus_client
+          '';
+
         };
       };
     };
